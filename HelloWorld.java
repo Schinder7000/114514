2908e912336095c98478750e2f1ed1de2ea3efe0
Schinder7000 <www.13022491219@outlook.com>
2025-04-02 20:38:35 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*多行注释
+这是我的第一个HelloWorld 应用程序
+使用的是多行注释，用绿色表示
+*/
+public class HelloWorld{
+	/*这是我们的主程序执行的入口 
+		main方法
+	*/
+	public static void main(String [] args){
+		//蓝色为关键字 单行注释 输出文本HelloWorld www.mayikt.com
+		System.out.println("HelloWorld www.mayikt.com");
+	}
+
+}
